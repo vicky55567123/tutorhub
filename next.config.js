115d23e00df657d383d601d1eee466dbc4369114
Ahmed Waqar <Ahmed.Waqar@bentley.com>
2025-07-12 22:36:34 +0500

    Fix webpack runtime error by simplifying Next.js config

--- next.config.js
+++ next.config.js
@@ -11,17 +11,6 @@ const nextConfig = {
       },
     ],
   },
-  // Fix CSS optimization issues in production builds
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      // Optimize CSS for production builds
-      config.optimization = {
-        ...config.optimization,
-        minimize: true,
-      }
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
