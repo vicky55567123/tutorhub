fa8e4bbd884d2fd2307205a4360c278fb7af2a7c
Ahmed Waqar <Ahmed.Waqar@bentley.com>
2025-07-29 23:31:21 +0500

    Revert to standard Next.js deployment for Netlify compatibility

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  distDir: 'out',
   images: {
     unoptimized: true,
     remotePatterns: [
